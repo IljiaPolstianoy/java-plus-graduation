b01d0bd50166d02b99f644aad1e5bb7566ae7b84
Илья <iljiafenix@yandex.ru>
2026-01-22 00:58:21 +0300

    fix

--- stats/stats-client/src/main/java/ru/practicum/stats/ClientRestStatImpl.java
+++ stats/stats-client/src/main/java/ru/practicum/stats/ClientRestStatImpl.java
@@ -1,16 +1,14 @@
 package ru.practicum.stats;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.util.UriComponentsBuilder;
-import org.springframework.web.util.UriUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.net.URI;
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
