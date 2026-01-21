be1a2c26f84ae77a327b67167e67b72f4347da47
Илья <iljiafenix@yandex.ru>
2026-01-22 01:13:24 +0300

    Delete .github directory

--- .github/stats/stats-client/src/main/java/ru/practicum/stats/error/StatsServerUnavailable.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.stats.error;
-
-public class StatsServerUnavailable extends RuntimeException {
-    public StatsServerUnavailable(String message, Exception cause) {
-        super(message, cause);
-    }
-}
