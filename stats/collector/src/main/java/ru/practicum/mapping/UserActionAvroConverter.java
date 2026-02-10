2212df3bb530f2e45cf2aa4504d3229dc8adc1af
Илья <iljiafenix@yandex.ru>
2026-02-10 03:10:32 +0300

    feat: добавлен сервис collector

---
+++ stats/collector/src/main/java/ru/practicum/mapping/UserActionAvroConverter.java
@@ -0,0 +1,49 @@
+package ru.practicum.mapping;
+
+import com.google.protobuf.Timestamp;
+import org.springframework.stereotype.Component;
+import ru.practicum.ewm.stats.avro.ActionTypeAvro;
+import ru.practicum.ewm.stats.avro.UserActionAvro;
+import stats.service.collector.ActionTypeProto;
+import stats.service.collector.UserActionProto;
+
+import java.time.Instant;
+
+import static ru.practicum.ewm.stats.avro.ActionTypeAvro.*;
+
+@Component
+public class UserActionAvroConverter {
+
+    public UserActionAvro toAvro(UserActionProto proto) {
+        return UserActionAvro.newBuilder()
+                .setUserId(proto.getUserId())
+                .setEventId(proto.getEventId())
+                .setActionType(toActionAvro(proto.getActionType()))
+                .setTimestamp(toInstant(proto.getTimestamp()))
+                .build();
+    }
+
+    private Instant toInstant(Timestamp timestamp) {
+        return Instant.ofEpochSecond(
+                timestamp.getSeconds(),
+                timestamp.getNanos()
+        );
+    }
+
+    private ActionTypeAvro toActionAvro(ActionTypeProto proto) {
+        switch (proto) {
+            case ACTION_LIKE -> {
+                return LIKE;
+            }
+            case ACTION_VIEW -> {
+                return VIEW;
+            }
+            case ACTION_REGISTER -> {
+                return REGISTER;
+            }
+            default -> {
+                return null;
+            }
+        }
+    }
+}
---
+++ stats/collector/src/main/java/ru/practicum/mapping/UserActionAvroSerialization.java
@@ -0,0 +1,37 @@
+package ru.practicum.mapping;
+
+import org.apache.avro.io.BinaryEncoder;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.specific.SpecificDatumWriter;
+import org.apache.kafka.common.serialization.Serializer;
+import ru.practicum.error.SerializationException;
+import ru.practicum.ewm.stats.avro.UserActionAvro;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class UserActionAvroSerialization implements Serializer<UserActionAvro> {
+
+    private final EncoderFactory encoderFactory = EncoderFactory.get();
+
+    @Override
+    public byte[] serialize(String topic, UserActionAvro data) {
+        if (data == null) {
+            return null;
+        }
+
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+
+            BinaryEncoder encoder = encoderFactory.binaryEncoder(outputStream, null);
+            DatumWriter<UserActionAvro> datumWriter = new SpecificDatumWriter<>(UserActionAvro.class);
+
+            datumWriter.write(data, encoder);
+            encoder.flush();
+
+            return outputStream.toByteArray();
+        } catch (IOException e) {
+            throw new SerializationException("Ошибка сериализации экземпляра SensorEventAvro", e);
+        }
+    }
+}
